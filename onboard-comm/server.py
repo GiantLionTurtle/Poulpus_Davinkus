fcdda17811d752b4a85b1d04094de291a71db69b
Vidieu <v.dieumegarde@hotmail.ca>
2025-01-22 09:19:50 -0500

    server code tcp

---
+++ onboard-comm/server.py
@@ -0,0 +1,25 @@
+import socket
+
+def main(host,port):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(1)
+    print(f"[*] Listening as {host}:{port}")
+
+    client_socket, client_address = s.accept()
+    print(f"[*] Connection from {client_address}")
+
+    while True:
+        data = client_socket.recv(1024).decode()
+        if not data:
+            break
+        print(f"[*] Data from client: {data}")
+        gcode_line = data.strip()
+        print(f"Received G-code: {gcode_line}")
+        client_socket.sendall(gcode_line)
+    client_socket.close()
+    s.close()
+
+if __name__ == "__main__":
+    main("ip_adress",5000)
